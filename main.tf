3cbfc18678c1f0ba99982d7e463236092f2fbf5b
Marcelo Mendonça <mendonca2709@gmail.com>
2022-08-08 20:30:14 -0300

    Update zone iterator

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
 # Cloudflare Firewall Rules
 # Firewall rules for all Zones Defined
 data "cloudflare_zones" "zones" {
-  count = length(var.domains)
-
   filter {
-    name   = var.domains[count.index]
-    paused = false
+    lookup_type = "exact"
+    name        = "sre.dapperlabs.com"
+    paused      = false
   }
 }
 
 # Cloudflare Filters needed to make a CF FW rule
 resource "cloudflare_filter" "filter" {
-  count = length(var.domains)
+  for_each = toset(var.domains)
 
-  zone_id     = lookup(data.cloudflare_zones.zones[count.index].zones[0], "id")
+  zone_id     = data.cloudflare_zones.zones.zones[0].id
   paused      = var.firewall_rule.paused
   expression  = var.firewall_rule.expression
   description = var.firewall_rule.description
@@ -22,10 +21,10 @@ resource "cloudflare_filter" "filter" {
 
 # Firewall Rule
 resource "cloudflare_firewall_rule" "rule" {
-  count = length(var.domains)
+  for_each = toset(var.domains)
 
-  zone_id     = lookup(data.cloudflare_zones.zones[count.index].zones[0], "id")
-  filter_id   = cloudflare_filter.filter[count.index].id
+  zone_id     = data.cloudflare_zones.zones.zones[0].id
+  filter_id   = cloudflare_filter.filter[each.value].id
   action      = var.firewall_rule.action
   paused      = var.firewall_rule.paused
   description = var.firewall_rule.description
--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "domains" {
   type        = list(string)
   description = "Cloudflare Domain to be applied to"
-  default     = []
 }
 
 variable "firewall_rule" {

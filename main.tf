9b87b1b843608b4e2946ba9d6371d79b33990d98
michaelgardner87 <michael.gardner@axiomzen.co>
2024-01-10 09:35:58 -0800

    tweak it

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "cloudflare_ruleset" "zone_level_waf_custom_rules" {
   zone_id     = lookup(data.cloudflare_zones.zones[count.index].zones[0], "id")
   name        = var.firewall_rule.name
   description = var.firewall_rule.description
-  kind        = "zone"
+  kind        = "custom"
   phase       = "http_request_firewall_custom"
 
   rules {

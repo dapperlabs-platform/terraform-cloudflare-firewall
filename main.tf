cf54eb19e903b903da7211c447365d256febc338
michaelgardner87 <michael.gardner@axiomzen.co>
2024-01-11 17:24:38 -0800

    remove dynamic block

--- main.tf
+++ main.tf
@@ -21,13 +21,7 @@ resource "cloudflare_ruleset" "zone_level_waf_custom_rules" {
   phase   = "http_request_firewall_custom"
 
   rules {
-    action = var.firewall_rule.action
-    dynamic "action_parameters" {
-      for_each = var.ruleset_id == null ? [] : [1]
-      content {
-        ruleset = var.ruleset_id
-      }
-    }
+    action      = var.firewall_rule.action
     expression  = var.firewall_rule.expression
     description = var.firewall_rule.description
     enabled     = var.firewall_rule.enabled

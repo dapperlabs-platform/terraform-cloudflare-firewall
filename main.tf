befa9fabfa5f1d81321a5f737308bf70e0a553ae
michaelgardner87 <michael.gardner@axiomzen.co>
2024-01-11 16:59:52 -0800

    add version

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ resource "cloudflare_ruleset" "zone_level_waf_custom_rules" {
       for_each = var.ruleset_id == null ? [] : [1]
       content {
         ruleset = var.ruleset_id
+        version = "current"
       }
     }
     expression  = var.firewall_rule.expression

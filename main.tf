fe53ceb0af79df09ff6123504e32c112a646c7ad
michaelgardner87 <michael.gardner@axiomzen.co>
2024-02-06 20:45:23 -0800

    remove the count

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ data "cloudflare_zones" "zones" {
 
 
 resource "cloudflare_ruleset" "zone_level_waf_custom_rules" {
-  zone_id = lookup(data.cloudflare_zones.zones[count.index].zones[0], "id")
+  zone_id = lookup(data.cloudflare_zones.zones.zones[0], "id")
   name    = "default"
   kind    = "zone"
   phase   = "http_request_firewall_custom"
